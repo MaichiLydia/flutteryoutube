556dd06bac6154febf1732cc90785e4c384cf460
Lydia Rodrigues <11950402+MaichiLydia@users.noreply.github.com>
2020-11-25 02:30:41 -0300

    10 app do curso

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertube/api.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
+import 'package:fluttertube/screens/home.dart';
+
+void main() {
+  Api api = Api();
+  api.search("teste");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: VideoBloc(),
+      child: MaterialApp(
+          title: 'FlutterTube',
+          debugShowCheckedModeBanner: false,
+          home: Home()
+      )
+    );
+  }
+}
+
+
